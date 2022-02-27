b534d8d004c7f608d3c3215f3fe7b8d247383ed7
Aidan Amato <aidanamato@comcast.net>
2022-02-27 14:20:59 -0500

    create misc folder in resources and add to .gitignore

--- src/main/java/com/revature/ers/Driver.java
+++ src/main/java/com/revature/ers/Driver.java
@@ -11,19 +11,12 @@ import com.revature.ers.services.UserService;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Driver {
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        UserService userService = new UserService(new UserDAO());
-
-        List<UserResponse> users = userService.getAllUsers();
-        System.out.println(users.toString());
-
-        try {
-            System.out.println(mapper.writeValueAsString(users));
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 1; i++ ) {
+            System.out.println(UUID.randomUUID());
         }
     }
 }
